3ee47c454b053a9ead236ff453d221c12f922dfb
Alex <alexanderb@perion.com>
2020-02-25 13:08:49 +0200

    First Commit

---
+++ src/test/java/com/undertone/meteringservice/MeteringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undertone.meteringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
